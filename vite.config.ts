361f2fb0bbb78ce9669d8c1893c5f9f074bc8f27
Lorran Monteiro <lorranmcm@gmail.com>
2022-05-10 15:14:24 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
+  plugins: [react()],
 });
